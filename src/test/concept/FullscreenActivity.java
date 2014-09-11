2d4f6b2e31314fc7636f2bd079ad082c72e9701b
Roger Smith <rogerlsmith@live.com>
2014-09-10 23:36:13 -0400

    last update

--- src/test/concept/FullscreenActivity.java
+++ src/test/concept/FullscreenActivity.java
@@ -652,32 +652,32 @@ public class FullscreenActivity extends Activity
   private Camera.Size getBestPreviewSize ( int width, int height, Camera.Parameters parameters )
   {
 	  
-    Camera.Size result=null;
-
-    for ( Camera.Size size : parameters.getSupportedPreviewSizes ( ) ) 
-    {
-      if ( size.width <= width && size.height <= height )
-      {
-        if ( result == null )
-        {
-          result = size;
-        }
-        else
-        {
-        	
-          int resultArea=result.width * result.height;
-          int newArea=size.width * size.height;
-
-          if ( newArea > resultArea )
-          {
-            result = size;
-          }
-          
-        }
-      }
-    }
-
-    return(result);
+	Camera.Size result=null;
+	
+	for ( Camera.Size size : parameters.getSupportedPreviewSizes ( ) ) 
+	{
+	  if ( size.width <= width && size.height <= height )
+	  {
+	    if ( result == null )
+	    {
+	      result = size;
+	    }
+	    else
+	    {
+	    	
+	      int resultArea=result.width * result.height;
+	      int newArea=size.width * size.height;
+	
+	      if ( newArea > resultArea )
+	      {
+	        result = size;
+	      }
+	      
+	    }
+	  }
+	}
+	
+	return(result);
   }
 
   
@@ -921,7 +921,7 @@ public class FullscreenActivity extends Activity
 	      }
 	      catch (java.io.IOException e)
 	      {
-	        Log.e("Concept", "Exception in photoCallback", e);
+	        Log.e ( "Concept", "Exception in photoCallback", e );
 	      }
 	
 	      return ( null );
